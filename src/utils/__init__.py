493486d397fef4f5e509ee310ebe9faa4ca2adda
Andrea Gasparini <andrea.gasparini11@gmail.com>
2022-10-11 19:33:25 +0200

    Updates PMI and adds load/save to dir methods

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
 from utils.common import list_to_dict
 from utils.wsd import SenseInventory, synsets_from_lemmapos, pos_map
 from utils.os import get_num_lines, get_basename, stem_basename_suffix
+from utils.metrics import PointwiseMutualInformation
--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,6 +1,8 @@
+import json
 import math
 from collections import Counter
 from functools import cache
+from typing import Dict, Optional
 
 from tqdm import tqdm
 
@@ -8,16 +10,29 @@ from utils.os import get_num_lines
 
 
 class PointwiseMutualInformation:
+    # serialization constants
+    # json attrs will get serialized into a single json file
+    JSON_ATTRS = ("source_file_path", "unigram_frequences", "bigram_frequences")
 
-    def __init__(self, source_file_path: str) -> None:
+    def __init__(self, source_file_path: str,
+                 unigram_frequences: Optional[Dict] = None,
+                 bigram_frequences: Optional[Dict] = None) -> None:
+
         self.source_file_path = source_file_path
-        self.unigram_frequences, self.bigram_frequences = Counter(), Counter()
-        self.__compute_frequences()
+
+        if unigram_frequences is None or bigram_frequences is None:
+            self.unigram_frequences = Counter()
+            self.bigram_frequences = Counter()
+            self.__compute_frequences()
+        else:
+            self.unigram_frequences = unigram_frequences
+            self.bigram_frequences = bigram_frequences
 
     def __call__(self, token1: str, token2: str) -> float:
         return self.pmi(token1, token2)
 
     def __compute_frequences(self) -> None:
+        """Computes unigram and bigram frequences from the source file."""
         with open(self.source_file_path) as source_file:
             for line in tqdm(source_file, total=get_num_lines(self.source_file_path)):
                 tokens = line.split(" ")
@@ -25,6 +40,17 @@ class PointwiseMutualInformation:
                     self.unigram_frequences[token] += 1
                     if len(tokens) > i + 1:
                         self.bigram_frequences[f"{token} {tokens[i + 1]}"] += 1
+
+    @property
+    def __serializable_attrs(self) -> Dict:
+        """Gets a dictionary of the attributes to serialize to json."""
+        json_attrs = dict()
+        for attr in self.JSON_ATTRS:
+            val = getattr(self, attr)
+            if val is None:
+                raise ValueError(f"attribute {attr} not found. Cannot serialize")
+            json_attrs[attr] = val
+        return json_attrs
 
     @cache
     def pmi(self, token1: str, token2: str) -> float:
@@ -50,3 +76,19 @@ class PointwiseMutualInformation:
             return -1
         prob_token1_token2 = self.bigram_frequences[f"{token1} {token2}"] / float(sum(self.bigram_frequences.values()))
         return self.pmi(token1, token2) / -math.log(prob_token1_token2, 2)
+
+    def save_to_dir(self, filepath: str) -> None:
+        """Saves to a directory."""
+        with open(filepath, "w") as f:
+            json.dump(self.__serializable_attrs, f)
+
+    @classmethod
+    def load_from_dir(cls, filepath: str) -> "PointwiseMutualInformation":
+        """Loads from a directory."""
+        with open(filepath) as f:
+            json_attrs = json.load(f)
+
+        if set(json_attrs.keys()) != set(cls.JSON_ATTRS):
+            raise ValueError(f"Expected serialized attributes to be {cls.JSON_ATTRS} but found {json_attrs.keys()}")
+
+        return cls(**json_attrs)
