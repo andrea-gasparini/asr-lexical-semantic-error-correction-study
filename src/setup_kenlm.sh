2b04f8abcd911a1d8d548580e8979296af2c50bf
Andrea Gasparini <andrea.gasparini11@gmail.com>
2022-10-27 16:38:02 +0200

    Updates setup kenlm script based on submodule

--- src/setup_kenlm.sh
+++ src/setup_kenlm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo apt install build-essential cmake libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libeigen3-dev zlib1g-dev libbz2-dev liblzma-dev -y
-wget -O - https://kheafield.com/code/kenlm.tar.gz | tar xz
+sudo apt install -y build-essential cmake libboost-all-dev zlib1g-dev libbz2-dev liblzma-dev
 mkdir kenlm/build && cd kenlm/build && cmake .. && make -j2
